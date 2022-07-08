65b43c19ae5c8da6fdd0216008114317103a9f98
sunji <2025506282@qq.com>
2022-07-08 17:38:09 +0800

    add

--- front-end/src/pages/cesium/CesiumPage.vue
+++ front-end/src/pages/cesium/CesiumPage.vue
@@ -8,6 +8,7 @@
   </div>
 </template>
 <script lang="ts">
+import { add } from "lodash"
 import { defineComponent, onMounted } from "vue"
 export default defineComponent({
   setup() {
@@ -19,50 +20,104 @@ export default defineComponent({
       init()
     })
     const addTiles = (viewer: any, url: string) => {
-      const result = viewer.scene.primitives.add(
-        new Cesium.Cesium3DTileset({
-          debugShowBoundingVolume: true,
-          debugColorizeTiles: true,
-          url: dataBaseUrl + url,
-          maximumScreenSpaceError: 2, // Temporary workaround for low memory mobile devices - Increase maximum error to 8.
-          maximumNumberOfLoadedTiles: 1000, // Temporary workaround for low memory mobile devices - Decrease (disable) tile cache.
-        }),
-      )
+      // 计算偏移量
+      // var translation = Cesium.Cartesian3.fromArray([114.33, 30.35, 10])
+      // var modelMatrix = Cesium.Matrix4.fromTranslation(translation)
+      const x = 114
+      const y = 30
+      const z = 10
+      // 创建Matrix4实例
+
+      const modelMatrix = Cesium.Matrix4.fromArray([
+        1.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        x,
+        y,
+        z,
+        1.0,
+      ])
+      const tileList = new Cesium.Cesium3DTileset({
+        // debugShowBoundingVolume: true,
+        // debugColorizeTiles: true,
+        url: dataBaseUrl + url,
+        maximumScreenSpaceError: 2, // Temporary workaround for low memory mobile devices - Increase maximum error to 8.
+        maximumNumberOfLoadedTiles: 1000, // Temporary workaround for low memory mobile devices - Decrease (disable) tile cache.
+        modelMatrix,
+      })
+      const result = viewer.scene.primitives.add(tileList)
+      // let count = 1
+      // result.tileLoad.addEventListener(function (tile: any) {
+      //   // console.log('一个瓦片加载了。');
+      //   count++
+      //   if (count === 7) {
+      //     // console.log("tile:", tile)
+      //     setTimeout(() => {
+      //       addVideo(viewer, "video", tile)
+      //     }, 3000)
+      //   }
+      // })
       result.readyPromise.then((tileset: any) => {
-        console.log(tileset, tileset.boundingSphere);
+        console.log(
+          "-----------------------------------------:",
+          tileset,
+          tileset.boundingSphere,
+          tileset.modelMatrix,
+        )
         // 将相机设置为查看新添加的tileset
         viewer.camera.viewBoundingSphere(
           tileset.boundingSphere,
           new Cesium.HeadingPitchRange(0, -0.5, 0),
         )
       })
-      viewer.flyTo(result)
+      // viewer.flyTo(result)
     }
-    const addVideo = (viewer: any, id: string) => {
-      const videoElement = document.getElementById(id)
+    const addVideo = (viewer: any, id: string, tile: any) => {
+      console.log("tile:", tile)
+      const result = viewer.entities.add({
+        name: "123",
+        position: Cesium.Cartesian3.fromDegrees(114, 30, 300000.0),
+        box: {
+          dimensions: new Cesium.Cartesian3(200.0, 200.0, 200.0),
+          material: Cesium.Color.GREEN.withAlpha(0.5),
+          outline: true,
+          outlineColor: Cesium.Color.RED,
+        },
+      })
+      // viewer.flyTo(result)
+      // const videoElement: any = document.getElementById(id)
       // 创建实体对象
       // 获取视频元素
       // 创建实体对象
-      const rectangle = viewer.entities.add({
-        rectangle: {
-          coordinates: Cesium.Rectangle.fromDegrees(-80.0, 39.0, -79.0, 40.0),
-          material: videoElement,
-        },
-        // 或创建多边形
-        // polygon: {
-        //    hierarchy: new PolygonHierarchy(positions),
-        //    material: videoElement
-        // },
-      })
-      // 锁定实体对象（这句可有可无）
-      viewer.trackedEntity = rectangle
-      new Cesium.VideoSynchronizer({
-        clock: viewer.clock,
-        element: videoElement,
-      })
-      viewer.clock.shouldAnimate = true
+      // const rectangle = viewer.entities.add({
+      //   rectangle: {
+      //     coordinates: Cesium.Rectangle.fromDegrees(116, 40, -100, 100),
+      //     material: videoElement,
+      //   },
+      //   // 或创建多边形
+      //   // polygon: {
+      //   //    hierarchy: new PolygonHierarchy(positions),
+      //   //    material: videoElement
+      //   // },
+      // })
+      // // 锁定实体对象（这句可有可无）
+      // viewer.trackedEntity = rectangle
+      // new Cesium.VideoSynchronizer({
+      //   clock: viewer.clock,
+      //   element: videoElement,
+      // })
+      // viewer.clock.shouldAnimate = true
       // videoElement.style.display = "none"
-      viewer.flyTo(rectangle)
+      // viewer.flyTo(rectangle)
     }
     const init = () => {
       const cesiumContainer = document.getElementById("cesiumContainer")
